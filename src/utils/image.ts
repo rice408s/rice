ecb79adf862e4daa598aef4bed2b922ebbc350ef
rice408s <107543976+rice408s@users.noreply.github.com>
2024-11-18 17:48:12 +0800

    blog

--- src/utils/image.ts
+++ src/utils/image.ts
@@ -1,12 +1,4 @@
 export function getProxiedImageUrl(url: string): string {
-  const cleanUrl = url.replace(/^http:\/\//, '');
-  
-  // 方案1：使用 statically.io
-  return `https://cdn.statically.io/img/${cleanUrl}`;
-  
-  // 方案2：使用 wsrv.nl（另一个配置）
-  // return `https://wsrv.nl/?url=http://${cleanUrl}`;
-  
-  // 方案3：使用 proxy.duckduckgo.com
-  // return `https://proxy.duckduckgo.com/iu/?u=http://${cleanUrl}`;
+  // 创建一个使用 HTTP 的 iframe 来加载图片
+  return `data:text/html,<iframe src="${url}" style="border:0;width:100%;height:100%"></iframe>`;
 } 
